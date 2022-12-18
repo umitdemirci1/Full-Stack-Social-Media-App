285b7d0dcd5ec78df970bf380955b139c487bd33
umitd111 <umit.demirci1996@gmail.com>
2022-12-18 19:45:34 +0300

    Initial commit

---
+++ src/test/java/com/example/QuestApp/QuestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
